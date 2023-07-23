a11bc903c0155c9b79fecab611538729dce51c72
Aparna <aparnaanair.106@gmail.com>
2023-07-23 18:07:56 +0530

    fix: "fixed login redirection"

--- src/components/common/Navbar/MenuData.jsx
+++ src/components/common/Navbar/MenuData.jsx
@@ -5,7 +5,6 @@ export const MenuData = [
     cName: "nav-links",
   },
 
-
   {
     title: "About",
     url: "#",
@@ -20,18 +19,7 @@ export const MenuData = [
 
   {
     title: "Login",
-    url: "http://localhost:5173/login",
+    url: "/login",
     cName: "nav-links",
   },
 ];
- 
-    
-    {
-        title: "Login",
-        url:"http://localhost:5173/login",
-        cName:"nav-links",
-    },
-
-
-]
-
